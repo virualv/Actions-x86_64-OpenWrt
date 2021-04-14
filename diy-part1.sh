3ddc5be82d2096684ccbf4dd378769a7baaa8dc6
virualv <42433288+virualv@users.noreply.github.com>
2021-04-14 23:14:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# config openclash
 mkdir package/luci-app-openclash
-
 pushd package/luci-app-openclash
 git init
 git remote add -f origin https://github.com/vernesong/OpenClash.git
@@ -26,29 +26,40 @@ pushd luci-app-openclash/tools/po2lmo
 make && sudo -E make install
 popd
 popd
+# config openclash end
 
-pushd feeds/packages/net
-git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
-popd
-pushd feeds/luci/applications
-git clone https://github.com/pymumu/luci-app-smartdns.git
-popd
+# config smartdns
+SMARTDNS_WORKING_DIR="`pwd`/feeds/packages/net/smartdns"
+mkdir $SMARTDNS_WORKING_DIR -p
+rm $SMARTDNS_WORKING_DIR/* -fr
+wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $SMARTDNS_WORKING_DIR/master.zip
+unzip $SMARTDNS_WORKING_DIR/master.zip -d $SMARTDNS_WORKING_DIR
+mv $SMARTDNS_WORKING_DIR/openwrt-smartdns-master/* $SMARTDNS_WORKING_DIR/
+rmdir $SMARTDNS_WORKING_DIR/openwrt-smartdns-master
+rm $SMARTDNS_WORKING_DIR/master.zip
+unset SMARTDNS_WORKING_DIR
 
-./scripts/feeds install -p package || true
-./scripts/feeds install -p luci || true
+SMARTDNS_LUCIBRANCH="lede" #更换此变量
+SMARTDNS_LUCI_WORKING_DIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+mkdir $SMARTDNS_LUCI_WORKING_DIR -p
+rm $SMARTDNS_LUCI_WORKING_DIR/* -fr
+wget https://github.com/pymumu/luci-app-smartdns/archive/${SMARTDNS_LUCIBRANCH}.zip -O $SMARTDNS_LUCI_WORKING_DIR/${SMARTDNS_LUCIBRANCH}.zip
+unzip $SMARTDNS_LUCI_WORKING_DIR/${SMARTDNS_LUCIBRANCH}.zip -d $SMARTDNS_LUCI_WORKING_DIR
+mv $SMARTDNS_LUCI_WORKING_DIR/luci-app-smartdns-${SMARTDNS_LUCIBRANCH}/* $SMARTDNS_LUCI_WORKING_DIR/
+rmdir $SMARTDNS_LUCI_WORKING_DIR/luci-app-smartdns-${SMARTDNS_LUCIBRANCH}
+rm $SMARTDNS_LUCI_WORKING_DIR/${SMARTDNS_LUCIBRANCH}.zip
+unset SMARTDNS_LUCIBRANCH
+unset SMARTDNS_LUCI_WORKING_DIR
+# config smartdns end
 
-mkdir -p package/extra
-pushd package/extra
-
-#git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#git clone https://github.com/pymumu/luci-app-smartdns
-#git clone https://github.com/kongfl888/luci-i18n-smartdns-zh-cn.git
-#git clone https://github.com/pymumu/openwrt-smartdns
+#
+pushd package/lean
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/virualv/luci-theme-pink.git
-
 popd
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+./scripts/feeds clean
